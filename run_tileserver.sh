a2ce5e97bd2749f34faf0910f2e44d026e36efcc
Kacper Golinski <kacper.golinski@gmail.com>
2022-09-30 10:38:43 +0200

    update velomap-grey style

--- run_tileserver.sh
+++ run_tileserver.sh
@@ -4,6 +4,6 @@ docker run -d --restart unless-stopped \
   -v "$(pwd)/data":/data \
   -v "$(pwd)/src/vector-tiles/sprites":/app/node_modules/tileserver-gl-styles/sprites \
   -v "$(pwd)/src/vector-tiles/styles/velomap":/app/node_modules/tileserver-gl-styles/styles/velomap \
-  -v "$(pwd)/src/vector-tiles/styles/positron":/app/node_modules/tileserver-gl-styles/styles/positron \
+  -v "$(pwd)/src/vector-tiles/styles/velomap-grey":/app/node_modules/tileserver-gl-styles/styles/velomap-grey \
   -p 127.0.0.1:8080:8080 \
   maptiler/tileserver-gl -p 8080
