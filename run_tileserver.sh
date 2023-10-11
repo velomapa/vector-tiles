6a87fd97864a2bc8cbe9a8fd370e4157a30674a8
Kacper Golinski <kacper.golinski@gmail.com>
2023-10-11 19:25:34 +0200

    update docker

--- run_tileserver.sh
+++ run_tileserver.sh
@@ -9,5 +9,6 @@ docker run -d --restart unless-stopped \
   -v "$(pwd)/src/vector-tiles/sprites":/app/node_modules/tileserver-gl-styles/sprites \
   -v "$(pwd)/src/vector-tiles/styles/velomap":/app/node_modules/tileserver-gl-styles/styles/velomap \
   -v "$(pwd)/src/vector-tiles/styles/velomap-grey":/app/node_modules/tileserver-gl-styles/styles/velomap-grey \
+  -v "$(pwd)/src/vector-tiles/styles/velomap-dark":/app/node_modules/tileserver-gl-styles/styles/velomap-dark \
   -p 127.0.0.1:8080:8080 \
   maptiler/tileserver-gl -p 8080
