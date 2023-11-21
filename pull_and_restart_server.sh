708310c2100b05843373b2bf16dfceb22dbc627c
Kacper Golinski <kacper.golinski@gmail.com>
2023-11-21 17:24:51 +0100

    update restart script

--- pull_and_restart_server.sh
+++ pull_and_restart_server.sh
@@ -3,5 +3,5 @@
 cd src/vector-tiles
 git pull
 cd ../..
-docker stop $(docker ps -a -q)
+docker stop $(docker ps -a -q --filter ancestor=maptiler/tileserver-gl)
 bash ./src/vector-tiles/run_tileserver.sh
