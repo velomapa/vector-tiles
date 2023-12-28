a7da5e2b5cae79d1ca553fb739afb0c962ad637e
Kacper Golinski <kacper.golinski@gmail.com>
2023-12-28 13:15:09 +0100

    add osm liberty

--- run_tileserver.sh
+++ run_tileserver.sh
@@ -9,6 +9,6 @@ docker run -d --restart unless-stopped \
   -v "$(pwd)/src/vector-tiles/sprites":/app/node_modules/tileserver-gl-styles/sprites \
   -v "$(pwd)/src/vector-tiles/styles/velomap":/app/node_modules/tileserver-gl-styles/styles/velomap \
   -v "$(pwd)/src/vector-tiles/styles/velomap-grey":/app/node_modules/tileserver-gl-styles/styles/velomap-grey \
-  -v "$(pwd)/src/vector-tiles/styles/velomap-dark":/app/node_modules/tileserver-gl-styles/styles/velomap-dark \
+  -v "$(pwd)/src/vector-tiles/styles/osm-liberty":/app/node_modules/tileserver-gl-styles/styles/osm-liberty \
   -p 127.0.0.1:8080:8080 \
   maptiler/tileserver-gl -p 8080
