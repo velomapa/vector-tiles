8d4e79f0c307416af1989a25c562bc186aa72c46
Kacper Golinski <kacper.golinski@gmail.com>
2025-06-15 20:51:06 +0200

    update

--- run_tileserver.sh
+++ run_tileserver.sh
@@ -18,7 +18,7 @@
 # -d deattached
 
 docker run -d --restart unless-stopped \
-  -v "$(pwd)/mounted-data":/data/mbtiles \
+  -v "~/mounted-data":/data/mbtiles \
   -v "$(pwd)/src/vector-tiles/data":/data \
   -p 127.0.0.1:8080:8080 \
   maptiler/tileserver-gl:v4.10.3 -p 8080
